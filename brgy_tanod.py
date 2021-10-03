4f12adfaa0369ce8e6ba6f24cb3f31b402d3b494
Chris Evangelio <cevangelio@users.noreply.github.com>
2021-10-03 17:41:51 +0800

    Update port

--- brgy_tanod.py
+++ brgy_tanod.py
@@ -1,2 +1,15 @@
 import pandas as pd
 import numpy as np
+from Pytrader_API_V1_06 import *
+import requests
+
+
+MT = Pytrader_API()
+list_symbols = ['AUDCAD', 'AUDCHF', 'AUDJPY', 'AUDNZD', 'AUDUSD', 'CADCHF', 'CADJPY', 'EURAUD', 'EURCAD', 'EURCHF', 'EURGBP', 'EURJPY', 'EURUSD', 'CHFJPY', 'GBPAUD', 'GBPCAD','GBPCHF', 'GBPJPY', 'GBPUSD', 'NZDCAD', 'NZDJPY', 'NZDUSD', 'USDCAD', 'USDCHF', 'USDJPY']
+symbols = {}
+for pair in list_symbols:
+    symbols[pair] = pair
+ports = [1122, 1125, 1127]
+port_dict = {1122:'FTMO', 1125:'FXCM', 1127:'GP'}
+
+con = MT.Connect(server='192.168.1.2', port=1125, instrument_lookup=symbols)
