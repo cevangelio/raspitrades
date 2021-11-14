2ee78135856adc535adb22e33502ac990cfeae5f
Chris Evangelio <cevangelio@users.noreply.github.com>
2021-11-14 11:26:27 +0800

    Updated for faster exec

--- brgy_tanod.py
+++ brgy_tanod.py
@@ -13,3 +13,5 @@ ports = [1122, 1125, 1127]
 port_dict = {1122:'FTMO', 1125:'FXCM', 1127:'GP'}
 
 con = MT.Connect(server='192.168.1.2', port=1125, instrument_lookup=symbols)
+
+positions = MT.Get_all_open_positions()
